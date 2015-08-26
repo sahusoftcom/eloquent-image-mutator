4b9ab886eeb11058a7f5081f308c88bc3197139e
Mrigank <mridul@sahusoft.com>
2015-08-26 12:54:20 +0530

    Trait modified to update and delete

--- src/EloquentImageMutatorTrait.php
+++ src/EloquentImageMutatorTrait.php
@@ -2,26 +2,12 @@
 
 namespace SahusoftCom\EloquentImageMutator;
 
+use SahusoftCom\EloquentImageMutator\Dist\ImageService;
+use SahusoftCom\EloquentImageMutator\Dist\ImageField;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
 trait EloquentImageMutatorTrait 
 {
-
-    public $imagine=null;
-
-    protected function getImagineObject() 
-    {
-        $library = \Config::get('image.library', 'gd');
-
-        if ($library == 'imagick')
-            $imagine = new \Imagine\Imagick\Imagine();
-        else if ($library == 'gmagick')
-            $imagine = new \Imagine\Gmagick\Imagine();
-        else if ($library == 'gd')
-            $imagine = new \Imagine\Gd\Imagine();
-        else
-            $imagine = new \Imagine\Gd\Imagine();
-
-        return $imagine;
-    }
 
     public function getAttributeValue($key)
     {
@@ -30,8 +16,12 @@ trait EloquentImageMutatorTrait
         if(in_array($key, $this->image_fields))
         {
             $value = $this->retrievePhotoFieldValue($key, $value);
+            if(!file_exists(public_path().$value->orignal->url)){
+                $value = ImageService::getImageObject();
+                $this->attributes[$key] = $value->toJson();
+            }
         }
-        
+
         return $value;
     }
 
@@ -39,242 +29,80 @@ trait EloquentImageMutatorTrait
     {
         if (in_array($key, $this->image_fields) && $value)
         {
-            return $this->setPhotoAttribute($key, $value);
+            switch (get_class($value)) {
+
+                case 'Symfony\Component\HttpFoundation\File\UploadedFile':
+                        return $this->setImageAttributeForUploadedFileObject($key, $value);
+                    break;
+                case 'SahusoftCom\EloquentImageMutator\Dist\ImageFieldLocal':
+                        return $this->setImageAttributeForImageFieldLocalObject($key, $value);
+                    break;
+                
+                // case 'value':
+                //     # code...
+                //     break;
+                
+            }
+
         }
 
         return parent::setAttribute($key, $value);
     }
 
+    public static function boot()
+    {
+        parent::boot();
+
+        static::updated(function($model)
+        {
+            $oldObject = $model->getOriginal();
+            $imageFields = $model->image_fields;
+            
+            if(count($imageFields) > 0) {
+                foreach ($imageFields as $key => $value) {
+                    $imageObject = ImageService::getImageObject($oldObject[$value]);
+                    if($imageObject && !empty($imageObject->orignal->url) && $model->$value->orignal->url != $imageObject->orignal->url) {
+                        $imageObject->delete();
+                    }
+                }
+            }
+
+        });
+
+        static::deleted(function($model)
+        {
+            $imageFields = $model->image_fields;
+            
+            if(count($imageFields) > 0) {
+                foreach ($imageFields as $key => $value) {
+                    $model->$value->delete();
+                }
+            }
+
+        });
+
+    }
+
     public function retrievePhotoFieldValue($key, $value)
     {
-        if(empty($value)) {
-
-            $dimensions = \Config::get('image.dimensions');
-
-            $stdClass = new \stdClass();
-            
-            foreach($dimensions as $key => $item) {
-
-                $stdClass->$item['4'] = null;
-            }
-        } else {
-
-            $result = $this->getAllTheSizes($value);
-            $stdClass = new \stdClass();
-            
-            foreach($result as $key => &$item) {
-
-                $stdClass->$key = asset('/'.\Config::get('image.upload_dir').'/'.$item['urn']);
-            }
-
-        }
-        
-        return $stdClass;
+        return ImageService::getImageObject($value);
     }
 
-    public function getANewFolder()
+    public function setImageAttributeForUploadedFileObject($key, $value)
     {
-        return  'user/'.date('Y/m/d/i/s');
+        $imageFieldObject = ImageService::uploadImage($key, $value);
+        $this->attributes[$key] = $imageFieldObject->toJson();
     }
 
-    public function getUploadStoragePath()
+    public function setImageAttributeForImageFieldLocalObject($key, $value)
     {
-        return base_path().'/'.\Config::get('image.assets_upload_path');
+        $imageFieldObject = ImageService::copyImage($key, $value);
+        $this->attributes[$key] = $imageFieldObject->toJson();
     }
 
-    public function getANewFileName($ext)
-    {
-        return $this->getANewFolder().'/'.str_random(16).'.'.$ext;
-    }
-
-    public function setPhotoAttribute($key, $value)
-    {
-
-        $destination = $this->getANewFileName($value->getClientOriginalExtension());
-
-        $value->move($this->getUploadStoragePath().'/'.dirname($destination), basename($destination));
-
-        $urn = $this->makeFromFile($destination, $value->getClientOriginalName());
-        
-        $this->attributes[$key] = ($urn === null) ? null : $urn;
-    }
-
-    public function makeFromFile($urn, $original_name = '', $title='')
-    {
-        if(!$original_name)
-            $original_name = basename($urn);
-
-        $absFile = $this->getUploadStoragePath().'/'.$urn;
-
-        $tempValue = 0;
-
-        $this->createDimensions($absFile);
-
-        return $urn;
-    }
-
-    public function createDimensions($url, $dimensions = array())
-    {
-        $defaultDimensions = \Config::get('image.dimensions');
-
-        if (is_array($defaultDimensions)) $dimensions = array_merge($defaultDimensions, $dimensions);
-
-        foreach ($dimensions as $dimension)
-        {
-            $width   = (int) $dimension[0];
-            $height  = isset($dimension[1]) ?  (int) $dimension[1] : $width;
-            $crop    = isset($dimension[2]) ? (bool) $dimension[2] : false;
-            $quality = isset($dimension[3]) ?  (int) $dimension[3] : \Config::get('image.quality');
-
-            $dest = dirname($url).'/'.$width.'x'.$height.($crop?'_crop':'').'/'.basename($url);
-
-            $img = $this->resize($url, $dest, $width, $height, $crop, $quality);
-
-        }
-    }
-
-    public function getAllTheSizes($url, $getImageSize = true)
-    {
-        $dimensions = array();
-
-        $defaultDimensions = \Config::get('image.dimensions');
-     
-        if (is_array($defaultDimensions))
-            $dimensions = array_merge($defaultDimensions, $dimensions);
-
-        $ret = array();
-
-        foreach ($dimensions as $dimension) {
-            // Get dimmensions and quality
-            $width   = (int) $dimension[0];
-            $height  = isset($dimension[1]) ?  (int) $dimension[1] : $width;
-            $crop    = isset($dimension[2]) ? (bool) $dimension[2] : false;
-            $quality = isset($dimension[3]) ?  (int) $dimension[3] : \Config::get('image.quality');
-
-            $info = pathinfo($url);
-
-            // Directories and file names
-            $fileName       = $info['basename'];
-            $sourceDirPath  = $this->getUploadStoragePath().'/'.$info['dirname'];
-            $sourceFilePath = $sourceDirPath.'/'.$fileName;
-            $targetDirName  = $width.'x'.$height.($crop ? '_crop' : '');
-            $targetDirPath  = $sourceDirPath.'/'.$targetDirName.'/';
-            $targetFilePath = $targetDirPath.$fileName;
-            //$targetUrl      = asset($info['dirname'].'/'.$targetDirName.'/'.$fileName);
-
-            $file = $this->getUploadStoragePath().'/'.$info['dirname'].'/'.$targetDirName.'/'.$fileName;
-
-            if($getImageSize) {
-                if(!file_exists($file)) {
-                    $width = 0;
-                    $height = 0;
-                }
-                else {
-                    
-                    list($width, $height) = getimagesize($file);
-                }
-            }
-
-            $ret[$dimension[4]] = [
-                'urn'=> $info['dirname'].'/'.$targetDirName.'/'.$fileName,
-                'width' => $width,
-                'height' => $height
-            ];
-
-        }
-
-        return $ret;
-    }
-
-    public function crop($x, $y, $width, $height)
-    {
-        $ext = \File::extension($this->urn);
-        $destinationImage = $this->getANewFileName($ext,true);
-
-        $destinationAbsImage = $this->getUploadStoragePath().'/'.$destinationImage;
-
-        $sourceAbsImage = $this->getUploadStoragePath().'/'.$this->urn;
-            $this->imageCrop($sourceAbsImage,$destinationAbsImage,$x,$y,$width,$height);
-
-        if(!\File::exists($destinationAbsImage)) {
-            throw new Exception("Image not cropped and saved");
-        }
-
-        $photo = $this->makeFromFile($destinationImage,true,'','Image File');
-
-        return $photo;
-    }
-
-    public function imageCrop($source, $destination, $x=0, $y=0, $width=1, $height=1, $quality=90)
-    {
-        if(!\File::exists($source))
-            throw new Exception("[IMAGE SERVICE] Source file does not exist");
-
-        $destinationFolder = dirname($destination);
-
-        if(!\File::isDirectory($destinationFolder))
-            \File::makeDirectory($destinationFolder, 0777, true);
-
-        list($imgWidth,$imgHeight) = getimagesize($source);
-
-        $cropXPixels = $imgWidth*$x;
-        $cropYPixels = $imgHeight*$y;
-        $cropWidthPixels = $width * $imgWidth;
-        $cropHeightPixels = $height * $imgHeight;
-
-        $point = new \Imagine\Image\Point($cropXPixels,$cropYPixels);
-        $box = new \Imagine\Image\Box($cropWidthPixels,$cropHeightPixels);
-
-        if(empty($imagine))
-            $imagine = $this->getImagineObject();
-
-        try {
-
-            $imagine->open($source)
-                    ->crop($point,$box)
-                    ->save($destination, array('quality' => $quality)); 
-
-        } catch (\Exception $e) {
-
-            \Log::error('[IMAGE SERVICE] Image crop Failed to crop image  [' . $e->getMessage() . ']');
-
-        }
-
-        return $destination;            
-    }
-
-    public function resize($source, $destination, $width = 100, $height = null, $crop = false, $quality = 90)
-    {
-        if(!\File::exists($source))
-            throw new Exception("[IMAGE SERVICE] Source file does not exist");
-
-        $destinationFolder = dirname($destination);
-
-        if(!\File::isDirectory($destinationFolder))
-            \File::makeDirectory($destinationFolder, 0777, true);
-
-        // Set the size
-        $size = new \Imagine\Image\Box($width, $height);
-
-        // Now the mode
-        $mode = $crop ? \Imagine\Image\ImageInterface::THUMBNAIL_OUTBOUND : \Imagine\Image\ImageInterface::THUMBNAIL_INSET;
-        
-        if(empty($imagine))
-            $imagine = $this->getImagineObject();
-
-        try {
-
-            $imagine->open($source)
-                ->thumbnail($size, $mode)
-                ->save($destination, array('quality' => $quality));
-    
-        } catch (\Exception $e) {
-
-            \Log::error('[IMAGE SERVICE] Image resize Failed to crop image  [' . $e->getMessage() . ']');
-
-        }
-
-        return $destination;
-    }
+    // public function setImageAttributeForUrlString($key, $value)
+    // {
+    //     return $this->setImageAttributeForFileObject($key, $value);
+    // }
 
 }
--- src/config/image.php
+++ src/config/image.php
@@ -6,6 +6,7 @@ return array(
     'assets_upload_path' => 'storage/app/uploads',
     'quality'     => 85,
     'dimensions'  => [
+        [null,null,true, 85, 'orignal'],
         ['50','50',true, 85, 'thumbnail'],
         ['160','120',false, 85, 'xsmall'],
         ['240','180',false, 85, 'small'],
